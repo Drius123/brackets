41452bb3e90a9db012ada0dbe79720ed4ca64e3b
Андрей <adovgulev111@gmail.com>
2023-01-26 19:13:23 +0300

    50%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let n = 0;
+	let m = 0;
+	let k = 0;
+	let p = 0;
+	str.split('').forEach(item => {
+	item == '(' ? n += 1 : item == ')' ? n -= 1 : item == '|' ? m += 1 : item == '[' ? k += 1 : item == ']' ? k -= 1 : item == '{' ? p += 1 : p -= 1;
+	})
+	return (n == 0 && m == 0 ) && ( k == 0 && p == 0 ) ? true : false;
 }
