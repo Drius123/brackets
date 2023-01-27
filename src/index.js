4fc3be891554ce7bbbef99b539c7ea67c71bf174
Андрей <adovgulev111@gmail.com>
2023-01-27 18:50:51 +0300

    feat: new test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-	let n = 0;
+	let value = '';
+	let n;
 	let m = 0;
-	let k = 0;
-	let p = 0;
+	bracketsConfig = bracketsConfig.flat().join('').split('');
 	str.split('').forEach(item => {
-	item == '(' ? n += 1 : item == ')' ? n -= 1 : item == '|' ? m += 1 : item == '[' ? k += 1 : item == ']' ? k -= 1 : item == '{' ? p += 1 : p -= 1;
-	})
-	return (n == 0 && m == 0 ) && ( k == 0 && p == 0 ) ? true : false;
+		bracketsConfig.indexOf(item) % 2 == 0 ? (value = bracketsConfig[bracketsConfig.indexOf(item) + 1], m += 1) : (item == value || value == '') ? (value = '', m -= 1) : n = false;
+	});
+	return n == undefined && m == 0 ? true : false;
 }
